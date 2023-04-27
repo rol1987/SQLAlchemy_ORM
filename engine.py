c15a54328922b7075a07ba302730e2e86fee5571
sergey korolev <rol1987@yandex.ru>
2023-04-27 15:57:21 +0300

    Initial commit5

--- engine.py
+++ engine.py
@@ -4,7 +4,7 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
-DSN = 'postgresql://postgres:83FrkWrt@localhost:5432/netology_db'
+DSN = 'postgresql://postgres:XXXXXXX@localhost:5432/netology_db'
 engine = create_engine(DSN)
 
 
